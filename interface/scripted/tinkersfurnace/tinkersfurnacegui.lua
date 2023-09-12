1e8eccbfbf4c1fc2998e307858fd1320cbb76f36
May <97141450+Natod@users.noreply.github.com>
2023-09-12 15:29:05 -0700

    please help im so confused

--- interface/scripted/tinkersfurnace/tinkersfurnacegui.lua
+++ interface/scripted/tinkersfurnace/tinkersfurnacegui.lua
@@ -3,16 +3,17 @@ require "/objects/tinkersfurnace/tinkersfurnace.lua"
 
 
 function init()
-    
+    local steamdecks = require "/objects/tinkersfurnace/tinkersfurnace.lua".storage.steamdeckcount
     self.canvas = widget.bindCanvas("canvas")
     local backTipBorder = {230,230,230,255}
-    self.canvas:drawRect({10, 10, 10 + storage.steamdeckcount, 20}, backTipBorder)
+    self.canvas:drawRect({10, 10, 10 + steamdecks, 20}, backTipBorder)
 end
+
 
 function update(dt)
     self.canvas:clear()
     --self.canvas:drawRect({10, 10, 10 + number, 20}, backTipBorder)
-    widget.setText("lblText2", "Steam Decks generated: " .. storage.steamdeckcount)
+    widget.setText("lblText2", "Steam Decks generated: " .. steamdecks)
     --number = number + 1
     self.canvas:drawText("Steam Deck", {position={math.random(1,99), math.random(1,99)}},20,{math.random(1,255), math.random(1,255),math.random(1,255),255})
     --self.canvas:drawLine(vec2.sub(vec2.mul(line[1], tileSize), screenPos), vec2.sub(vec2.mul(line[2], tileSize), screenPos), anchorColor, 2)
