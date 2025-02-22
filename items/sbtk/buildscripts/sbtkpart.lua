df48f65e030295e2046405f71e1f0bbf27ed9792
Mayyyyy <97141450+Natod@users.noreply.github.com>
2025-02-22 03:18:33 -0800

    it was difficult to put the pieces together

--- items/sbtk/buildscripts/sbtkpart.lua
+++ items/sbtk/buildscripts/sbtkpart.lua
@@ -6,7 +6,7 @@ function build(directory, config, parameters, level, seed)
 
   local sbtkMatTable = root.assetJson(sbtkSourceJson)["sbtkToolMatConfig"][matType]
   local sbtkPartTable = root.assetJson(sbtkSourceJson)["sbtkPartConfig"][partType]
-  local sbtkPartInfo = sbtkMatTable.partsConfig[partType]
+  local sbtkPartInfo = sbtkMatTable.partsConfig[partType] or root.assetJson(sbtkSourceJson)["partsConfigDefault"][partType]
   --sb.logInfo(sb.print(sbtkPartInfo))
   
   local baseColors = root.assetJson(sbtkSourceJson)["baseColors"]
@@ -15,16 +15,18 @@ function build(directory, config, parameters, level, seed)
   local shortdesc = sbtkMatTable.label .. " " .. sbtkPartTable.label
 
   local desc = string.format("Material: ^#%s;", sbtkMatTable.textColor) .. sbtkMatTable.desc .. 
+    ((partType == "toolcore") and ("\nTier " .. sbtkMatTable.tier) or "") ..
     "^reset;\n" .. sbtkPartInfo.desc .. 
-    "\nDamage: " .. sbtkPartInfo.damage
+    ((sbtkPartInfo.damage ~= nil) and ("\nDamage: " .. sbtkPartInfo.damage) or "")
 
   local img = sbtkPartTable.inventoryIcon .. string.format(
-    "?replace;%s=%s;%s=%s;%s=%s;%s=%s;%s=%s",
+    "?replace;%s=%s;%s=%s;%s=%s;%s=%s;%s=%s;%s=%s",
     baseColors[1], matColors[1], --darkest
-    baseColors[2], matColors[2],
+    baseColors[2], matColors[2], 
     baseColors[3], matColors[3],
-    baseColors[4], matColors[4], --lightest
-    baseColors[5], matColors[5] --glow outline
+    baseColors[4], matColors[4],
+    baseColors[5], matColors[5], --lightest
+    baseColors[6], matColors[6] --glow outline
   )
 
   parameters.shortdescription = shortdesc
