732cf8c1480f5baae132f92e30ff072ddf552084
Mayyyyy <97141450+Natod@users.noreply.github.com>
2025-02-23 05:14:11 -0800

    parts category

---
+++ items/sbtk/buildscripts/sbtkweapon.lua
@@ -0,0 +1,9 @@
+require "/scripts/util.lua"
+require "/scripts/vec2.lua"
+require "/scripts/versioningutils.lua"
+require "/scripts/staticrandom.lua"
+require "/items/buildscripts/abilities.lua"
+
+function build(directory, config, parameters, level, seed)
+  
+end
