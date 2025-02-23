f504efc1ef775352b631bca4bd0d7b028ed24d0a
12090113 <47487266+12090113@users.noreply.github.com>
2025-02-23 01:32:25 -0800

    Revert "failing to pass lua to lua"
    This reverts commit 4e8e4f1879f8ea25c8539caebe75145e1725a343.

--- interface/scripted/tinkersfurnace/tinkersfurnacegui.lua
+++ interface/scripted/tinkersfurnace/tinkersfurnacegui.lua
@@ -1,19 +1,18 @@
 require "/scripts/vec2.lua"
-require "/objects/tinkersfurnace/tinkersfurnace.lua"
 
+local number = 1
 
 function init()
-    
     self.canvas = widget.bindCanvas("canvas")
     local backTipBorder = {230,230,230,255}
-    self.canvas:drawRect({10, 10, 10 + storage.steamdeckcount, 20}, backTipBorder)
+    self.canvas:drawRect({10, 10, 10 + number, 20}, backTipBorder)
 end
 
 function update(dt)
     self.canvas:clear()
     --self.canvas:drawRect({10, 10, 10 + number, 20}, backTipBorder)
-    widget.setText("lblText2", "Steam Decks generated: " .. storage.steamdeckcount)
-    --number = number + 1
+    widget.setText("lblText2", "Steam Decks generated: " .. number)
+    number = number + 1
     self.canvas:drawText("Steam Deck", {position={math.random(1,99), math.random(1,99)}},20,{math.random(1,255), math.random(1,255),math.random(1,255),255})
     --self.canvas:drawLine(vec2.sub(vec2.mul(line[1], tileSize), screenPos), vec2.sub(vec2.mul(line[2], tileSize), screenPos), anchorColor, 2)
 end
